0da5ea54ea8166c4ab3cb59092e9faa73ee4c425
Sricharan2211 <tudugani.charan@mphasis.com>
2023-12-29 16:06:32 +0530

    phase 2 assignment 15

---
+++ WebAPIDataManagement/WebAPIDataManagement/Models/Teams.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIDataManagement.Models
+{
+    [Table("Teams")]
+    public class Teams
+    {
+        [Key]
+        public int TId {  get; set; }
+        [Required]
+        [StringLength(50)]
+        public string TName { get; set; }
+        [Required]
+        public string TEmail {  get; set; }
+        [Required]
+        [StringLength (50)]
+        public string TSlogan {  get; set; }
+    }
+}
